c765c7fba4113510f40e351f037341d5ffb0bbb5
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-02 22:38:35 +0100

    fix errors

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *pivot, *tmp;
 
-	if (!tree || !tree->right)
+	if (!tree || !tree->left)
 		return (NULL);
 
 	/* tree and pivot exchange childes */
