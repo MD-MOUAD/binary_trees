109a621842d36b7018a3757722c8eba6153f84cd
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-02 06:48:23 +0100

    fix try this

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -93,12 +93,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 		btree_node = dequeue(queue);
 		left = btree_node->left;
 		right = btree_node->right;
-		if (!left && right)
-		{
-			free2(queue);  /* If a node has a left child*/
-			return (0);   /* but no right child */
-		}
-		if (!left && !right)
+		if (!left || !right) /* order is important */
 		{
 			while (queue->front)
 			{
@@ -109,26 +104,64 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 					return (0);
 				}
 			}
-			free2(queue);
 			return (1);
 		}
-		if (left && !right && (left->left || left->right))
-		{
-			free2(queue);  /* If a node has a left child but no right child */
-			return (0);  /* and is not in the last level */
-		}
-		if (left && !right && last_except == 1)
-		{
-			free2(queue);  /* If a node has a left child but ni right child */
-			return (0);  /* and is in the last level for the second time */
-		}
-		if (left && !right)
-			last_except = 1;
-		if (left)
-			enqueue(queue, left);
-		if (right)
-			enqueue(queue, right);
-	}
+		/* order is important */
+		enqueue(queue, left);
+		enqueue(queue, right);
 	free2(queue);
 	return (1);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 		if (!left && right)
+// 		{
+// 			free2(queue);  /* If a node has a left child*/
+// 			return (0);   /* but no right child */
+// 		}
+// 		if (!left && !right)
+// 		{
+// 			while (queue->front)
+// 			{
+// 				btree_node = dequeue(queue);
+// 				if (btree_node->left || btree_node->right)
+// 				{
+// 					free2(queue);
+// 					return (0);
+// 				}
+// 			}
+// 			free2(queue);
+// 			return (1);
+// 		}
+// 		if (left && !right && (left->left || left->right))
+// 		{
+// 			free2(queue);  /* If a node has a left child but no right child */
+// 			return (0);  /* and is not in the last level */
+// 		}
+// 		if (left && !right && last_except == 1)
+// 		{
+// 			free2(queue);  /* If a node has a left child but ni right child */
+// 			return (0);  /* and is in the last level for the second time */
+// 		}
+// 		if (left && !right)
+// 			last_except = 1;
+// 		if (left)
+// 			enqueue(queue, left);
+// 		if (right)
+// 			enqueue(queue, right);
