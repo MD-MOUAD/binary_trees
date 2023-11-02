1011e28212111ff32b95f2441f06c453ceacf9ec
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-02 21:52:46 +0100

    add prototype to header

--- binary_trees.h
+++ binary_trees.h
@@ -116,5 +116,10 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 /* ========== 103-binary_tree_rotate_left.c ========== */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
+/* ========== 104-binary_tree_rotate_right.c ========== */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+
+/* */
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
