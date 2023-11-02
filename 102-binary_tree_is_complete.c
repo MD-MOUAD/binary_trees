b850522de9680e712635c8146f4dfe43458b1dc6
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-02 07:32:28 +0100

    try

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -83,13 +83,11 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
 	queue = malloc(sizeof(Queue));
 	if (!queue)
 		return (0);
 	queue->front = queue->rear = NULL;
 	enqueue(queue, tree);
-
 	while (queue->front)
 	{
 		btree_node = dequeue(queue);
@@ -121,53 +119,3 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	free2(queue);
 	return (1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 		if (!left && right)
-// 		{
-// 			free2(queue);  /* If a node has a left child*/
-// 			return (0);   /* but no right child */
-// 		}
-// 		if (!left && !right)
-// 		{
-// 			while (queue->front)
-// 			{
-// 				btree_node = dequeue(queue);
-// 				if (btree_node->left || btree_node->right)
-// 				{
-// 					free2(queue);
-// 					return (0);
-// 				}
-// 			}
-// 			free2(queue);
-// 			return (1);
-// 		}
-// 		if (left && !right && (left->left || left->right))
-// 		{
-// 			free2(queue);  /* If a node has a left child but no right child */
-// 			return (0);  /* and is not in the last level */
-// 		}
-// 		if (left && !right && last_except == 1)
-// 		{
-// 			free2(queue);  /* If a node has a left child but ni right child */
-// 			return (0);  /* and is in the last level for the second time */
-// 		}
-// 		if (left && !right)
-// 			last_except = 1;
-// 		if (left)
-// 			enqueue(queue, left);
-// 		if (right)
-// 			enqueue(queue, right);
