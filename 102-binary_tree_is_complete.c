b10d7c75792185916c718fff7e3ca71b0af45907
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-02 06:04:18 +0100

    last fix

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -90,25 +90,35 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	enqueue(queue, tree);
 	while (queue->front)
 	{
+		btree_node = dequeue(queue);
 		left = btree_node->left;
 		right = btree_node->right;
-		btree_node = dequeue(queue);
 		if (!left && right)
 		{
 			free2(queue);  /* If a node has a left child*/
 			return (0);   /* but no right child */
+		}
+		if (!left && ! right)
+		{
+			while (queue->front)
+			{
+				btree_node = dequeue(queue);
+				if (btree_node->left || btree_node->right)
+					return (0);
+			}
+			return (1);
 		}
 		if (left && !right && (left->left || left->right))
 		{
 			free2(queue);  /* If a node has a left child but no right child */
 			return (0);  /* and is not in the last level */
 		}
-		if ((left && !right || !left && !right) && last_except == 1)
+		if (left && !right && last_except == 1)
 		{
-			free2(queue);  /* If a node has no childs or only a left child */
+			free2(queue);  /* If a node has a left child but ni right child */
 			return (0);  /* and is in the last level for the second time */
 		}
-		if (left && !right || !left && !right)
+		if (left && !right)
 			last_except = 1;
 		if (left)
 			enqueue(queue, left);
