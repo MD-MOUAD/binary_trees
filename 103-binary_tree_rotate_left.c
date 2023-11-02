c5540ae7b4364678c550e016cc09329a0e2559b2
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-02 21:43:59 +0100

    103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,36 +1,32 @@
+#include "binary_trees.h"
 
-		if (!left && right)
-		{
-			free2(queue);  /* If a node has a left child*/
-			return (0);   /* but no right child */
-		}
-		if (!left && !right)
-		{
-			while (queue->front)
-			{
-				btree_node = dequeue(queue);
-				if (btree_node->left || btree_node->right)
-				{
-					free2(queue);
-					return (0);
-				}
-			}
-			free2(queue);
-			return (1);
-		}
-		if (left && !right && (left->left || left->right))
-		{
-			free2(queue);  /* If a node has a left child but no right child */
-			return (0);  /* and is not in the last level */
-		}
-		if (left && !right && last_except == 1)
-		{
-			free2(queue);  /* If a node has a left child but ni right child */
-			return (0);  /* and is in the last level for the second time */
-		}
-		if (left && !right)
-			last_except = 1;
-		if (left)
-			enqueue(queue, left);
-		if (right)
-			enqueue(queue, right);
+/**
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree: a pointer to the root node of the tree to rotate
+ * Return: a pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *pivot, *tmp;
+
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	if (tmp)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
+	return (pivot);
+}
