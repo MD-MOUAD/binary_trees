85a4b56e0e829e8aa73599001f2f230011467e07
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-03 04:12:20 +0100

    betty style

--- binary_trees.h
+++ binary_trees.h
@@ -123,20 +123,50 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 /* ========== 104-binary_tree_rotate_right.c ========== */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
-/* */
+/* ========== 110-binary_tree_is_bst.c ==========*/
 int binary_tree_is_bst(const binary_tree_t *tree);
+
+/* ========== 111-bst_insert.c ========== */
 bst_t *bst_insert(bst_t **tree, int value);
+
+/* ========== 112-array_to_bst.c ========== */
 bst_t *array_to_bst(int *array, size_t size);
+
+/* ========== 113-bst_search.c ========== */
 bst_t *bst_search(const bst_t *tree, int value);
+
+/* ========== 114-bst_remove.c ========== */
 bst_t *bst_remove(bst_t *root, int value);
+
+/* ========== 120-binary_tree_is_avl.c ========== */
 int binary_tree_is_avl(const binary_tree_t *tree);
+
+/* ========== 121-avl_insert.c ========== */
 avl_t *avl_insert(avl_t **tree, int value);
+
+/* ========== 122-array_to_avl.c ========== */
 avl_t *array_to_avl(int *array, size_t size);
+
+/* ========== 123-avl_remove.c ========== */
 avl_t *avl_remove(avl_t *root, int value);
+
+/* ========== 124-sorted_array_to_avl.c ========== */
 avl_t *sorted_array_to_avl(int *array, size_t size);
+
+/* ========== 130-binary_tree_is_heap.c ========== */
 int binary_tree_is_heap(const binary_tree_t *tree);
+
+/* ========== 131-heap_insert.c ========== */
 heap_t *heap_insert(heap_t **root, int value);
+
+/* ========== 132-array_to_heap.c ========== */
 heap_t *array_to_heap(int *array, size_t size);
+
+/* ========== 133-heap_extract.c ========== */
 int heap_extract(heap_t **root);
+
+/* ========== 134-heap_to_sorted_array.c ========== */
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+
 #endif /* BINARY_TREES_H */
